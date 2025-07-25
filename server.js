0dd8b65e7fd3e7cd51d5b02ed906cc1aaa679a48
vetri1323 <vetrigp@gmail.com>
2025-07-25 08:12:47 +0530

    Update server.js

--- server.js
+++ server.js
@@ -37,8 +37,7 @@ const allowedOrigins = [
   'http://127.0.0.1:3000',
   'http://localhost:5000',
   'http://127.0.0.1:5000',
-  'https://celebrated-kashata-1a90d0.netlify.app',
-  'https://ganesaeseva-backend.onrender.com'
+  'https://celebrated-kashata-1a90d0.netlify.app'
 ];
 
 // CORS configuration
